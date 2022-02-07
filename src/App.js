d9245a3112bba9372608707f3d655184bb3b81e6
Ameya Deshmukh <0xameya@pop-os.localdomain>
2022-02-07 10:00:11 +0530

    Detecting a Solana object injected by Phantom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
+import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
+
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { solana } = window;
+		if(solana){
+			if(solana.isPhantom){
+				console.log('Phantom wallet found!');
+			}
+			
+
+			else{
+				alert('Solana object not found! Get a Phantom Wallet!');
+			}
+		}
+		}
+		catch(error){
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		const onLoad = async () => {
+			await checkIfWalletIsConnected();
+		};
+		window.addEventListener('load', onLoad);
+		return () => window.removeEventListener('load', onLoad);
+	}, []);
+
   return (
     <div className="App">
       <div className="container">
